eae14d36b5710d24cffd8fcf59db0138d25e47b2
determaer <determaer2012@gmail.com>
2024-12-13 16:07:54 +0700

    template groups

--- src/data/templates.ts
+++ src/data/templates.ts
@@ -9,35 +9,35 @@ export const templates = {
     },
     {
       "templateId": "template149",
-      "groupId": 'group2',
+      "groupId": 'group1',
       "type": "TEXT",
       "title": "Приветствие 1",
       "template": "Добрый день, {{1}}!\nОтличный день, чтобы {{2}}. Не хотите ли сходить в {{3}}? Проверить {{4}}? Реализовать {{5}}? Все вопросы задавайте в чат {{6}}",
     },
     {
       "templateId": "template159",
-      "groupId": 'group2',
+      "groupId": 'group3',
       "type": "IMAGE",
       "title": "Фото объекта",
       "template": "Добрый день, {{1}}!\nОтличный день, чтобы {{2}}. Во вложении фото будущего объекта. Как вам?",
     },
     {
       "templateId": "template169",
-      "groupId": 'group2',
+      "groupId": 'group3',
       "type": "VIDEO",
       "title": "Видеопрезентация",
       "template": "Добрый день, {{1}}!\nОтправляю вам видеопрезентацию нашего проекта. У нас действует специальная акция {{2}}. Сообщите, пожалуйста, о своем решении",
     },
     {
       "templateId": "template179",
-      "groupId": 'group2',
+      "groupId": 'group3',
       "type": "DOCUMENT",
       "title": "Наш прайс",
       "template": "Добрый день, {{1}}!\nОтправляю вам наш прайс. Возможно вам будет интересна {{2}}. Если будут вопросы, пожалуйста, напишите мне",
     },
     {
       "templateId": "template181",
-      "groupId": 'group1',
+      "groupId": 'group2',
       "type": "TEXT",
       "title": "Напоминание об оплате",
       "template": "Добрый день, {{1}}!\nНапоминаем о предстоящем платеже на сумму {{2}}, который необходимо внести до {{3}}.\nПожалуйста, подтвердите актуальность этой информации или сообщите, если произошли изменения.\nБудем рады вашему ответу!",
@@ -62,7 +62,7 @@ export const templates = {
     },
     {
       "templateId": "template182",
-      "groupId": 'group1',
+      "groupId": 'group2',
       "type": "TEXT",
       "title": "Напоминание об оплате",
       "template": "Добрый день, {{1}}!\nНапоминаем о предстоящем платеже на сумму {{2}}. По вопросам обращайтесь по контактам",
@@ -83,7 +83,7 @@ export const templates = {
     },
     {
       "templateId": "template183",
-      "groupId": 'group1',
+      "groupId": 'group3',
       "type": "DOCUMENT",
       "title": "Счёт и напоминание об оплате",
       "template": "Добрый день, {{1}}!\nНапоминаем о предстоящем платеже на сумму {{2}}. По вопросам обращайтесь по контактам",
@@ -108,12 +108,17 @@ export const templates = {
     {
       groupId: 'group1',
       iconUrl: 'https://placehold.jp/30/336633/ffffff/64x64.png?text=grp1',
-      title: 'my group 1',
+      title: 'Ввод',
     },
     {
       groupId: 'group2',
       iconUrl: 'https://placehold.jp/30/336633/ffffff/64x64.png?text=grp2',
-      title: 'my group 2',
+      title: 'Кнопки ответа',
+    },
+    {
+      groupId: 'group3',
+      iconUrl: 'https://placehold.jp/30/336633/ffffff/64x64.png?text=grp3',
+      title: 'Файлы',
     },
   ],
 };
