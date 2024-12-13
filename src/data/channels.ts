f045d1b0ff7765a2f42979feeaf74cf243523ef8
determaer <determaer2012@gmail.com>
2024-12-13 15:31:35 +0700

    waba icon

--- src/data/channels.ts
+++ src/data/channels.ts
@@ -2,7 +2,7 @@ export const channels = [
   { 
     channelId: 'channelWABA', 
     title: 'WhatsApp', 
-    icon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Telegram_logo.svg/240px-Telegram_logo.svg.png' 
+    icon:  './waba-chat-demo/public/waba.svg'
   },
   { 
     channelId: 'channelSMS', 
